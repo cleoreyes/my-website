cc8b0601b2dfcdafc4c2165edafeaceb276fb772
Cleo <121153199+cleoreyes@users.noreply.github.com>
2025-01-02 22:10:52 -0800

    fixed nav

--- my-app/src/app/components/navbar.tsx
+++ my-app/src/app/components/navbar.tsx
@@ -7,7 +7,7 @@ export default function NavBar() {
   const pathname = usePathname();
 
   return (
-    <nav className="navbar flex flex-row justify-between items-center md:rounded-full drop-shadow-2xl bg-neutral-800 md:px-8 py-2 md:py-4 md:mx-40 text-white text-xs md:text-xl font-light fixed top-0 left-0 right-0 z-50 overflow-x-auto">
+    <nav className="navbar flex flex-row justify-between items-center md:rounded-full drop-shadow-2xl bg-neutral-800 md:px-8 py-2 md:py-4 md:mx-40 text-white text-xs md:text-xl md:mt-10 font-light fixed top-0 left-0 right-0 z-50 overflow-x-auto">
       <div>
         <Link
           className={`navLink ${pathname === "/" ? "active" : ""}`}
