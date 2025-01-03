5337641dd6b3151b10071e625ddf18d75685105a
Cleo <121153199+cleoreyes@users.noreply.github.com>
2025-01-02 20:57:56 -0800

    responsive navbar

--- my-app/src/app/components/navbar.tsx
+++ my-app/src/app/components/navbar.tsx
@@ -4,10 +4,10 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 export default function NavBar() {
-  const pathname = usePathname(); // Get the current route
+  const pathname = usePathname();
 
   return (
-    <nav className="navbar flex flex-col md:flex-row justify-between items-center rounded-full drop-shadow-2xl bg-neutral-800 px-8 py-4 mt-10 mx-40 text-white text-xl font-light fixed top-0 left-0 right-0 z-50">
+    <nav className="navbar flex flex-row md:justify-between items-center md:rounded-full drop-shadow-2xl bg-neutral-800 md:px-8 py-2 md:py-4 md:mx-40 text-white text-xs md:text-xl font-light fixed top-0 left-0 right-0 z-50 overflow-x-auto">
       <div>
         <Link
           className={`navLink ${pathname === "/" ? "active" : ""}`}
@@ -16,33 +16,34 @@ export default function NavBar() {
           Cleo Reyes
         </Link>
       </div>
-      <div>
-        <div className="flex flex-col md:flex-row justify-between items-center gap-4 md:gap-10">
-          <Link
-            className={`navLink ${pathname === "/projects" ? "active" : ""}`}
-            href="/projects"
-          >
-            Projects
-          </Link>
-          <Link
-            className={`navLink ${pathname === "/experience" ? "active" : ""}`}
-            href="/experience"
-          >
-            Experience
-          </Link>
-          <Link
-            className={`navLink ${pathname === "/about" ? "active" : ""}`}
-            href="/about"
-          >
-            About
-          </Link>
-          {/* <Link
-            className={`navLink ${pathname === "/blog" ? "active" : ""}`}
-            href="/blog"
-          >
-            Blog
-          </Link> */}
-        </div>
+
+      <div
+        className={`flex flex-row md:flex-row justify-between items-center gap-2 md:gap-10 md:flex md:block`}
+      >
+        <Link
+          className={`navLink ${pathname === "/projects" ? "active" : ""}`}
+          href="/projects"
+        >
+          Projects
+        </Link>
+        <Link
+          className={`navLink ${pathname === "/experience" ? "active" : ""}`}
+          href="/experience"
+        >
+          Experience
+        </Link>
+        <Link
+          className={`navLink ${pathname === "/about" ? "active" : ""}`}
+          href="/about"
+        >
+          About
+        </Link>
+        {/* <Link
+          className={`navLink ${pathname === "/blog" ? "active" : ""}`}
+          href="/blog"
+        >
+          Blog
+        </Link> */}
       </div>
     </nav>
   );
